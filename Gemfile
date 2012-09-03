eb34362d4ecd67a748c01c5bd60ca845cf6e0f19
Robert Kenny <kenoir@gmail.com>
2012-09-03 14:26:55 +0100

    trying to copy heroku reference

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 gem 'sinatra'
 gem 'rest-client'
+
 gem 'rack'
 gem 'rack-test'
 gem 'rspec'
--- app.rb
+++ app.rb
@@ -1,9 +1,5 @@
 require 'sinatra'
-require 'rest_client'
 
 get '/hi' do
-  RestClient.proxy = ENV['http_proxy']
-  response = RestClient.get "http://juicer.responsivenews.co.uk/api/events/7"
-
-  response.inspect
+  "Hello World!"
 end
