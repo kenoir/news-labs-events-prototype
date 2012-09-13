e10be43e0deaf2d6218b80277ba07d155c80ae99
Claudio Neves <claudio.neves@bbc.co.uk>
2012-09-13 12:05:44 +0100

    Truncation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,11 @@
-require(["./sliderise","./mapfeed"], function(sliderise,mapfeed) {
+require(["./sliderise","./mapfeed", "./truncate", "./jquery.jtruncate"], function(sliderise,mapfeed,truncate,jtruncate) {
         sliderise('section.people');
         sliderise('section.places');
         sliderise('section.articles');
         sliderise('section.timeline');
 
-        mapfeed('section.places');        
-        //truncate('section.people ul.info');
+        mapfeed('section.places');
+        
+        truncate('section.people ul.info');
 });
 
