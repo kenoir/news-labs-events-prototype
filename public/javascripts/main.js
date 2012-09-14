19f9b0584631c15421aae4b1bf8caa6a0ec6522b
Robert Kenny <kenoir@gmail.com>
2012-09-14 13:43:22 +0100

    hotfix for js loading

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,12 @@
 require(["./sliderise", "./truncate", "./jquery.jtruncate"], function(sliderise,truncate,jTruncate) {
-        sliderise('section.people');
-        sliderise('section.places');
-        sliderise('section.articles');
-        sliderise('section.timeline');
+  $(document).ready(function(){
+    sliderise('section.people');
+    sliderise('section.places');
+    sliderise('section.articles');
+    sliderise('section.timeline');
 	
-        truncate('div#news-lab-events article.intro p#event-description');        
-        truncate('section.people ul.info li');
+    truncate('div#news-lab-events article.intro p#event-description');        
+    truncate('section.people ul.info li');
+  });
 });
 
