0174dd014111954b4c6bcf0aef75bad8e7891e43
Robert Kenny <kenoir@gmail.com>
2012-10-02 17:07:05 +0100

    started test refactor

--- spec/controllers/news_events_page_controller_spec.rb
+++ spec/controllers/news_events_page_controller_spec.rb
@@ -1,74 +1,61 @@
-describe NewsEventsPageController, "#initialize" do
-  it 'should accept and set an event ID' do
-    events_controller = NewsEventsPageController.new(dummy_id)
-    events_controller.event_api_base_path = dummy_event_api_base_path
+describe NewsEventsPageController do
 
-    events_controller.id.should == dummy_id
-  end
-  
-  it 'should accept and set an event_api_base_path' do
-    events_controller = NewsEventsPageController.new(dummy_id)
-    events_controller.event_api_base_path = dummy_event_api_base_path
+  subject { NewsEventsPageController.new( dummy_id ) }
 
-    events_controller.event_api_base_path.should == dummy_event_api_base_path 
-  end
+  describe NewsEventsPageController, "#initialize" do
+    it 'should accept and set an event ID' do
+      subject.id.should == dummy_id
+    end
 
-  it 'should set rest_client as RestClient' do
-    events_controller = NewsEventsPageController.new(dummy_id)
-    events_controller.event_api_base_path = dummy_event_api_base_path
+    it 'should set builder as an instance of Builder' do
+      controller = NewsEventsPageController.new(dummy_id)
+      controller.builder.should be_an_instance_of(Builder)
+    end
 
-    events_controller.rest_client.should == RestClient 
   end
 
-  it 'should set builder as an instance of Builder' do
-    events_controller = NewsEventsPageController.new(dummy_id)
-    events_controller.event_api_base_path = dummy_event_api_base_path
+  describe NewsEventsPageController, "#run" do
 
-    events_controller.builder.should be_an_instance_of(Builder)
+    it 'should call populate then build_array_of_type in the set builder' do
+      controller = NewsEventsPageController.new(dummy_id)
+      controller.event_api_base_path = dummy_event_api_base_path
+      controller.event_base_path = dummy_event_base_path
+      controller.rest_client = dummy_rest_client
+
+      builder = double('Builder')
+
+      builder.should_receive(:populate).exactly(3).times
+      builder.should_receive(:build_array_of_type).any_number_of_times
+
+      controller.builder = builder
+      controller.run!
+    end
+
+    it 'should return a hash containing :event and set the event' do
+      controller = NewsEventsPageController.new(dummy_id)
+      controller.event_api_base_path = dummy_event_api_base_path
+      controller.event_base_path = dummy_event_base_path
+      controller.rest_client = dummy_rest_client
+
+      builder = double('Builder') 
+      builder.stub(:populate)
+      builder.stub(:build_array_of_type)
+
+      controller.builder = builder
+
+      page = controller.run!
+      page[:event].should be_an_instance_of(Event)
+    end
+
   end
 
-end
+  describe NewsEventsPageController, "#events_uri" do
+    it 'should return an events_uri when passed an id' do
+      controller = NewsEventsPageController.new(dummy_id)
+      controller.event_base_path = dummy_event_base_path
 
-describe NewsEventsPageController, "#run" do
-
-  it 'should call populate then build_array_of_type in the set builder' do
-    events_controller = NewsEventsPageController.new(dummy_id)
-    events_controller.event_api_base_path = dummy_event_api_base_path
-    events_controller.event_base_path = dummy_event_base_path
-    events_controller.rest_client = dummy_rest_client
-
-    builder = double('Builder')
-
-    builder.should_receive(:populate).exactly(3).times
-    builder.should_receive(:build_array_of_type).any_number_of_times
-
-    events_controller.builder = builder
-    events_controller.run!
+      controller.events_uri.should == dummy_event_base_path + dummy_id.to_s
+    end
   end
 
-  it 'should return a hash containing :event and set the event' do
-    events_controller = NewsEventsPageController.new(dummy_id)
-    events_controller.event_api_base_path = dummy_event_api_base_path
-    events_controller.event_base_path = dummy_event_base_path
-    events_controller.rest_client = dummy_rest_client
-
-    builder = double('Builder') 
-    builder.stub(:populate)
-    builder.stub(:build_array_of_type)
-
-    events_controller.builder = builder
-
-    page = events_controller.run!
-    page[:event].should be_an_instance_of(Event)
-  end
-
-end
-
-describe NewsEventsPageController, "#events_uri" do
-  it 'should return an events_uri when passed an id' do
-    events_controller = NewsEventsPageController.new(dummy_id)
-    events_controller.event_base_path = dummy_event_base_path
-
-    events_controller.events_uri.should == dummy_event_base_path + dummy_id.to_s
-  end
 end
