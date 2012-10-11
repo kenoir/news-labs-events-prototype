d9755ea622db9e197e519f29351ac5251ebca6d2
Robert Kenny <kenoir@gmail.com>
2012-10-11 10:04:44 +0100

    article placeholder page

--- mixin/loggable.rb
+++ mixin/loggable.rb
@@ -1,6 +1,13 @@
 module Loggable
 
   def log(title,e)
+    if Application.environment.to_s == "development" ||
+       Application.environment.to_s == "test"
+
+      puts "~~ #{title} ~~"
+      return
+    end
+
     puts "\n"
     puts "----------------------------------------------------------------\n"
     puts "#{title}\n"
--- spec/controllers/news_articles_page_spec.rb
+++ spec/controllers/news_articles_page_spec.rb
@@ -3,8 +3,10 @@ describe NewsArticlesPageController do
   subject { NewsArticlesPageController.new(dummy_id) }
 
   describe NewsArticlesPageController, "#run" do
-    page = subject.run!
-    page.should have_key(:article)
+    it 'should return a hash with the subject key :article' do
+      page = subject.run!
+      page.should have_key(:article)
+    end
   end
 
 end
